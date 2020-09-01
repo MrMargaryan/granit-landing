19b424ab078ab6e3df30e2166166f6e55e37c62b
MrMargaryan <hamlet.java@gmail.com>
2020-09-01 23:47:52 +0300

    done

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const toggleButton = document.querySelector('.main-nav--toggle')
+const menu = document.querySelector('.main-nav--menu')
+
+let isOpen = false
+
+toggleButton.addEventListener('click', () => {
+  if (isOpen) {
+    isOpen = false
+    menu.style.display = 'none'
+    toggleButton.style.backgroundImage = 'url("assets/hamburger-menu.svg")'
+  } else {
+    isOpen = true
+    menu.style.display = 'block'
+    toggleButton.style.backgroundImage = 'url("assets/close.svg")'
+  }
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 801) {
+    isOpen = false
+    menu.style.display = 'none'
+    toggleButton.style.backgroundImage = 'url("assets/hamburger-menu.svg")'
+  }
+})
